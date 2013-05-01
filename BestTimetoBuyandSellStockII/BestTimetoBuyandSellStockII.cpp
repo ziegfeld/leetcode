16d3aa0ffa83153e06a05cc096b65abdaac22288
anson <anson627@gmail.com>
2013-04-30 20:09:22 -0700

    correct description

--- BestTimetoBuyandSellStockII/BestTimetoBuyandSellStockII.cpp
+++ BestTimetoBuyandSellStockII/BestTimetoBuyandSellStockII.cpp
@@ -1,13 +1,13 @@
 //============================================================================
-// Best Time to Buy and Sell Stock
+// Best Time to Buy and Sell Stock II
 //
-// Say you have an array for which the ith element is the price of a given
+// Say you have an array for which the ith element is the price of a given 
 // stock on day i.
 //
-// If you were only permitted to complete at most one transaction (ie, buy
-// one and sell one share of the stock), design an algorithm to find the
-// maximum profit.
-//
+// Design an algorithm to find the maximum profit. You may complete as many 
+// transactions as you like (ie, buy one and sell one share of the stock 
+// multiple times). However, you may not engage in multiple transactions at 
+// the same time (ie, you must sell the stock before you buy again).
 //============================================================================
 
 #include <iostream>
