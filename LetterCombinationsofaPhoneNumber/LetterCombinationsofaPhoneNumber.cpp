4a06c68ddf46a4d1ed70ea2bbeb400ad4fdc343b
unknown <biqian@ANSON-T430S.redmond.corp.microsoft.com>
2013-12-21 13:17:52 -0800

    fix bug

--- LetterCombinationsofaPhoneNumber/LetterCombinationsofaPhoneNumber.cpp
+++ LetterCombinationsofaPhoneNumber/LetterCombinationsofaPhoneNumber.cpp
@@ -9,31 +9,35 @@
 // in any order you want.
 //============================================================================
 
+#include <iostream>
 #include <string>
 #include <vector>
+
 using namespace std;
 
 class Solution {
 public:
     static const string keypad[8];
-    vector<string> result;
+
     vector<string> letterCombinations(string digits) {
-        result.clear();
-        letterCombinationsHelper(digits, 0, "");
-        return result;
+        vector<string> res;
+        letterCombinationsHelper(digits, 0, "", res);
+        return res;
     }
 
-    void letterCombinationsHelper(string& digits, size_t i, string str) {
-        if (i == digits.size()) {
-            result.push_back(str);
+    void letterCombinationsHelper(string& digits, int i, string str, vector<string> & res) {
+        if (i == (int)digits.size()) {
+            res.push_back(str);
+            return;
         }
         int pos = digits[i] - '2';
-        for (size_t j = 0; j < keypad[pos].size(); j++) {
-            letterCombinationsHelper(digits, i + 1, str + keypad[pos][j]);
+        for (int j = 0; j < (int)keypad[pos].size(); j++) {
+            letterCombinationsHelper(digits, i + 1, str + keypad[pos][j], res);
         }
     }
 };
-const string Solution::keypad[8]  = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+const string Solution::keypad[8] = { "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
 
 int main() {
     return 0;
