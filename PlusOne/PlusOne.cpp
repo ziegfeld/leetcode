29b53c97d8a9b5b8b1978553dd38725cee588599
Apollo Zhi <ziegfeld@users.noreply.github.com>
2014-02-20 13:00:39 -0500

    Update PlusOne.cpp

--- PlusOne/PlusOne.cpp
+++ PlusOne/PlusOne.cpp
@@ -13,19 +13,19 @@ using namespace std;
 class Solution {
 public:
     vector<int> plusOne(vector<int> &digits) {
-        int N = digits.size();
-        for (int i = N - 1; i >= 0; i--) {
+        //vector<int> res = digits;
+        int i = digits.size() - 1;
+        for (; i >= 0; i--) {
             if (digits[i] < 9) {
                 digits[i]++;
-                return;
+                break;
             }
-            else {
-                digits[i] = 0;
-            }
+            else digits[i] = 0;
         }
-        vector<int> res(N + 1, 0);
-        res[0] = 1;
-        return res;
+        // learn this: digits.insert(digits.begin(), 1);
+        if (i == -1)
+            digits.insert(digits.begin(), 1);
+        return digits;
     }
 };
 
