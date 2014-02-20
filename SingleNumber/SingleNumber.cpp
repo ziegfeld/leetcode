e0de10a7a762f652cf7aac15afc289f631105e38
Apollo Zhi <ziegfeld@users.noreply.github.com>
2014-02-20 11:52:44 -0500

    Update SingleNumber.cpp

--- SingleNumber/SingleNumber.cpp
+++ SingleNumber/SingleNumber.cpp
@@ -15,7 +15,8 @@ class Solution {
 public:
     int singleNumber(int A[], int n) {
         int res = 0;
-        for (int i = 0; i < n; i++) res ^= A[i];
+        for (int i = 0; i < n; i++)
+            res ^= A[i];
         return res;
     }
 };
