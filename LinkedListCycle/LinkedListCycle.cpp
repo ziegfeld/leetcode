10961ad2bd5da9a8e53126516fdc4158dcf3c93b
Apollo Zhi <ziegfeld@users.noreply.github.com>
2014-02-20 11:51:35 -0500

    Update LinkedListCycle.cpp

--- LinkedListCycle/LinkedListCycle.cpp
+++ LinkedListCycle/LinkedListCycle.cpp
@@ -19,15 +19,18 @@ struct ListNode {
     ListNode *next;
     ListNode(int x) : val(x), next(NULL) {}
 };
-
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode * fastNode = head, * slowNode = head;
-        while (fastNode != NULL && fastNode->next != NULL) {
-            fastNode = fastNode->next->next;
-            slowNode = slowNode->next;
-            if (fastNode == slowNode) return true;
+        ListNode * runner1 = head;
+        ListNode * runner2 = head;
+        while (runner2 != NULL){
+            runner1 = runner1 -> next;
+            runner2 = runner2 -> next;
+            if (runner2 == NULL || (runner2 = runner2 -> next) == NULL) 
+                return false;
+            else if (runner1 == runner2)
+                return true;
         }
         return false;
     }
