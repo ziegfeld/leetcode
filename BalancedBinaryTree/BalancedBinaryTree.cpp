4c3c10dd7392f823977e1021ee006eba8def9a2a
Apollo Zhi <ziegfeld@users.noreply.github.com>
2014-02-20 11:50:26 -0500

    Update BalancedBinaryTree.cpp

--- BalancedBinaryTree/BalancedBinaryTree.cpp
+++ BalancedBinaryTree/BalancedBinaryTree.cpp
@@ -29,20 +29,7 @@ struct TreeNode {
 class Solution {
 public:
     bool isBalanced(TreeNode *root) {
-        return isBalanced2(root);
-    }
-
-    bool isBalanced1(TreeNode *cur) {
-        if (cur == NULL) return true;
-        return (abs(height(cur->left) - height(cur->right)) <= 1) && isBalanced(cur->left) && isBalanced1(cur->right);
-    }
-
-    int height(TreeNode * cur) {
-        if (cur == NULL) return 0;
-        return max(height(cur->left), height(cur->right)) + 1;
-    }
-
-    bool isBalanced2(TreeNode * root) {
+        //return isBalanced_queue(root);
         return (isBalancedHelper(root) >= 0);
     }
 
@@ -51,6 +38,38 @@ public:
         int lh = isBalancedHelper(cur->left), rh = isBalancedHelper(cur->right);
         if (lh == -1 || rh == -1 || abs(lh - rh) > 1) return -1;
         return max(lh, rh) + 1;
+    }
+
+    // wrong answer by me! for example:
+    // { 1; 2,2 ; 3,3,3,3;4,4,4,4,4,4,#,#; 5,5 is balanced!! but false negative here.
+    bool isBalanced_queue(TreeNode *root) {
+        queue<TreeNode*> qs;
+        if (!root) return true;
+        qs.push(root);
+        qs.push(NULL);
+
+        int totalOfLevel = 0;
+        int totalShouldOfLevel = 1;
+        
+        while (!qs.empty()){
+            TreeNode * cur = qs.front();
+            qs.pop();
+            
+            if (cur) {
+                totalOfLevel ++;
+                if (cur->left)  qs.push(cur->left);
+                if (cur->right) qs.push(cur->right);
+            } else if (!qs.empty()) {
+                // level ends, tree still goes.
+                if (totalShouldOfLevel != totalOfLevel)
+                    return false;
+                
+                totalShouldOfLevel <<= 1;
+                totalOfLevel = 0;
+                qs.push(NULL);
+            }
+        }
+        return true;
     }
 };
 
