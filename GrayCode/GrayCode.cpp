f8ff3334328af2e27526ecc8e7725e33cd0a8a88
Apollo Zhi <ziegfeld@users.noreply.github.com>
2014-02-20 11:57:26 -0500

    Update GrayCode.cpp

--- GrayCode/GrayCode.cpp
+++ GrayCode/GrayCode.cpp
@@ -25,18 +25,18 @@ class Solution {
 public:
     vector<int> grayCode(int n) {
         vector<int> res;
-        res.reserve(1 << n);
+        res.reserve(1<<n);
         res.push_back(0);
-        for (int i = 0; i < n; i++) {
-            int m = res.size();
-            for (int j = m - 1; j >= 0; j--) {
-                res.push_back(res[j] | (1 << i));
-            }
+        int highest = 1;
+        for (int i = 0; i < n; i++){
+            int size = res.size();
+            for (int j = size - 1; j >= 0; j--)
+                res.push_back(res[j] | highest);
+            highest <<= 1;
         }
         return res;
     }
 };
-
 int main() {
 
     Solution sol;
