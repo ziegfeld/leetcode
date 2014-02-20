a14b14a9590e3c477697884cecff482beb9534a3
Apollo Zhi <ziegfeld@users.noreply.github.com>
2014-02-20 11:53:35 -0500

    Update BestTimetoBuyandSellStockII.cpp

--- BestTimetoBuyandSellStockII/BestTimetoBuyandSellStockII.cpp
+++ BestTimetoBuyandSellStockII/BestTimetoBuyandSellStockII.cpp
@@ -20,13 +20,11 @@ using namespace std;
 
 class Solution {
 public:
-    int maxProfit(vector<int> & prices) {
-        int res = 0;
-        for (size_t i = 1; i < prices.size(); i++) {
-            int d = prices[i]-prices[i-1];
-            if (d > 0) res += d;
-        }
-        return res;
+    int maxProfit(vector<int> &prices) {
+        int retval = 0;
+        for (size_t i = 1; i< prices.size(); i++) 
+            retval += (prices[i]-prices[i-1]>0)? (prices[i]-prices[i-1]) : 0;
+        return retval;
     }
 };
 
