f318f15f02850f7b0fef1a9f61b3bc09be7f2507
Apollo Zhi <ziegfeld@users.noreply.github.com>
2014-02-20 13:00:19 -0500

    Update Permutations.cpp

--- Permutations/Permutations.cpp
+++ Permutations/Permutations.cpp
@@ -13,39 +13,63 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int> > permute(vector<int> &num) {
-        return permute2(num);
-    }
-
-    vector<vector<int> > permute1(vector<int> & num) {
-        sort(begin(num), end(num));
-        vector<vector<int> > res;
-        do {
-            res.push_back(num);
-        } while (next_permutation(begin(num), end(num)));
-        return res;
-
-    }
-
-    vector<vector<int> > permute2(vector<int> & num) {
-        vector<vector<int> > res;
-        permuteHelper2(num, 0, res);
+        // int n = num.size();
+        // if (n<=1) return 1
+        // sort(num.begin(),num.end());
+        // int repeat = 0;
+        // for (int i = 0; i < num.size(); i++){
+        vector<vector<int>> res;
+        permuteHelper(num, 0, res);
         return res;
     }
-
-    void permuteHelper2(vector<int> & num, int i, vector<vector<int> > & res) {
-        if (i == (int)num.size()) {
+    
+    void permuteHelper(vector<int> & num, int begin, vector<vector<int>> & res;) {
+        int end = num.size();
+        if (begin == end) {
             res.push_back(num);
             return;
         }
-
-        for (int j = i; j < (int)num.size(); j++) {
-            swap(num[i], num[j]);
-            permuteHelper2(num, i + 1, res);
-            swap(num[i], num[j]);
+        for (int cur = begin; cur < end; cur++) {
+            swap(num[begin],num[cur]);
+            permuteHelper(num, begin+1, res);
+            swap(num[begin],num[cur]);
         }
+        
     }
-};
+    
+    //     vector<vector<int> > permute(vector<int> &num) {
+    //     return permute2(num);
+    // }
 
+    // vector<vector<int> > permute1(vector<int> & num) {
+    //     sort(begin(num), end(num));
+    //     vector<vector<int> > res;
+    //     do {
+    //         res.push_back(num);
+    //     } while (next_permutation(begin(num), end(num)));
+    //     return res;
+
+    // }
+
+    // vector<vector<int> > permute2(vector<int> & num) {
+    //     vector<vector<int> > res;
+    //     permuteHelper2(num, 0, res);
+    //     return res;
+    // }
+
+    // void permuteHelper2(vector<int> & num, int begin, vector<vector<int> > & res) {
+    //     int end = num.size();
+    //     if (begin == end) {
+    //         res.push_back(num);
+    //         return;
+    //     }
+    //     for (int cur = begin; cur < end; cur++) {
+    //         swap(num[begin], num[cur]);
+    //         permuteHelper2(num, begin + 1, res);
+    //         swap(num[begin], num[cur]);
+    //     }
+    // }
+};
 int main() {
     Solution sol;
     vector<int> p0 = { 1, 2, 3 };
