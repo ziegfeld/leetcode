9e9b8d23cd5905778181b1714b784073ababc8ea
Apollo Zhi <ziegfeld@users.noreply.github.com>
2014-02-20 11:58:45 -0500

    Update IntegertoRoman.cpp

--- IntegertoRoman/IntegertoRoman.cpp
+++ IntegertoRoman/IntegertoRoman.cpp
@@ -9,22 +9,67 @@
 
 using namespace std;
 
-int ints[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-string romans[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+char table[] = {
+    'I',  //  1
+    'V', //  5
+    'X', // 10
+    'L', // 50
+    'C', //100
+    'D', // 500
+    'M'  //1000
+};
+
+int ints[] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+string romans[] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
 
 class Solution {
 public:
     string intToRoman(int num) {
-        string res;
-        int i = 0;
-        while (num > 0) {
+        return intToRoman_anson627(num);
+    }
+    
+    string intToRoman_anson627(int num) {
+        ostringstream os;
+        for (int i = 0; i < 13; i++) {
             while (num >= ints[i]) {
                 num -= ints[i];
-                res += romans[i];
+                os << romans[i];
             }
-            i++;
+        }
+        return os.str();
+    }
+    
+    
+
+    string intToRoman_My(int num) {
+        string res = "";
+        int weight = 10, i = 0;
+        while (num>0){
+            int_to_r_10(i++, num%10, res);
+            num /= 10;
         }
         return res;
+    }
+    void int_to_r_10(int i, int digit, string &res){
+        string val = "";
+        if (digit>8) {
+            // *val ++ = table[i * 2 ];热死 //1
+            // *val ++ = table[i * 2 + 2]; //10
+            val += table[i * 2 ];    // 1
+            val += table[i * 2 + 2]; //10
+        } else if (digit>=5) {
+            val += table[i * 2 + 1]; // 5
+            while (digit-- >5)
+                val += table[i * 2]; // 1
+        } else if (digit==4) {
+            val += table[i * 2 ];    // 1
+            val += table[i * 2 + 1]; // 5
+        } else {
+            while (digit-- >0)
+                val += table[i * 2]; // 1
+        }
+        res = val + res;
     }
 };
 
