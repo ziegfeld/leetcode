45642a088abcf4d33bb2b85752085b74fc75d9b6
Apollo Zhi <ziegfeld@users.noreply.github.com>
2014-02-20 11:56:38 -0500

    Update ClimbingStairs.cpp

--- ClimbingStairs/ClimbingStairs.cpp
+++ ClimbingStairs/ClimbingStairs.cpp
@@ -9,61 +9,28 @@
 // DP, O(n) time, O(1) space
 // Matrix Pow, O(log(n)) time, O(1) space
 //============================================================================
-
 class Solution {
 public:
-    int climbStairs(int n) {
-        return climbStairs3(n);
-    }
-
     int climbStairs1(int n) {
-        vector<int> dp(n + 1, 0);
-        dp[0] = dp[1] = 1;
-        for (int i = 2; i <= n; i++) dp[i] = dp[i - 1] + dp[i - 2];
-        return dp[n];
-    }
-
-    int climbStairs2(int n) {
-        int t1 = 1, t2 = 1, res = 1;
-        for (int i = 2; i <= n; i++) {
-            res = t1 + t2;
-            t1 = t2;
-            t2 = res;
+        if (n<=2) return n;
+        int curMin1 = 1;
+        int curMin2 = 2;
+        int i = 3;
+        int res = 0;
+        while (i<=n) {
+            res = curMin2 + curMin1;
+            curMin1 = curMin2;
+            curMin2 = res;
+            i++;
         }
         return res;
     }
-
-    int climbStairs3(int n) {
-        vector<vector<int> > m = {
-            { 1, 1 },
-            { 1, 0 }
-        };
-        m = pow(m, n);
-        return m[1][0] + m[1][1];
-    }
-
-    vector<vector<int> > pow(vector<vector<int> > & m, int n) {
-        if (n == 1) return m;
-        auto t = pow(m, n / 2);
-        t = mul(t, t);
-        if (n % 2 == 1) t = mul(t, m);
-        return t;
-    }
-
-    vector<vector<int> > mul(vector<vector<int> > & a, vector<vector<int> > & b) {
-        int n = a.size();
-        vector<vector<int> > c(n, vector<int>(n, 0));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < n; k++) {
-                    c[i][j] += a[i][k] * b[k][j];
-                }
-            }
-        }
-        return c;
+    
+    
+    int climbStairs(int n) {
+        climbStairs1(n);
     }
 };
-
 int main() {
     return 0;
 }
