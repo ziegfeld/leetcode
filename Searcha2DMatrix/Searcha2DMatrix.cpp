3044478b32c9299c1445f48ffd6f9b126c43387e
Apollo Zhi <ziegfeld@users.noreply.github.com>
2014-02-20 13:17:49 -0500

    Update Searcha2DMatrix.cpp

--- Searcha2DMatrix/Searcha2DMatrix.cpp
+++ Searcha2DMatrix/Searcha2DMatrix.cpp
@@ -24,23 +24,112 @@
 #include <vector>
 
 using namespace std;
-
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int> > & matrix, int target) {
+    bool searchMatrix(vector<vector<int> > &matrix, int target) {
+        // int m = matrix.size() - 1, n = matrix[0].size() - 1;
+        // if (target > matrix[m][n]) return false;
+        // int m1 = 0, n1 = 0;
+        return searchMatrix2(matrix,target);
+        //return searchBlock(matrix, target, m1, n1, m, n);
+    }
+    
+    //textbook solution
+    //http://leetcode.com/2010/10/searching-2d-sorted-matrix-part-ii.html
+    //"step-wise Linear Search" (starting with the upper right corner and traverse only either to the left or the bottom.)
+    bool searchMatrix2(vector<vector<int> > & matrix, int target) {
+        int M = matrix.size(), N = matrix[0].size();
+        int i = 0, j = N - 1;
+        while (i < M && j >= 0) {
+            if (matrix[i][j] == target) return true;
+            if (matrix[i][j] < target) i++;
+            else j--;
+        }
+        return false;
+    }
+
+bool binPart(int mat[][N_MAX], int M, int N, int target, int l, int u, int r, int d, int &targetRow, int &targetCol) {
+  if (l > r || u > d) return false;
+  if (target < mat[u][l] || target > mat[d][r]) return false;
+  int mid = l + (r-l)/2;
+  int row = u;
+  while (row <= d && mat[row][mid] <= target) {
+    if (mat[row][mid] == target) {
+      targetRow = row;
+      targetCol = mid;
+      return true;
+    }
+    row++;
+  }
+  return binPart(mat, M, N, target, mid+1, u, r, row-1, targetRow, targetCol) ||
+         binPart(mat, M, N, target, l, row, mid-1, d, targetRow, targetCol);
+}
+ 
+bool binPart(int mat[][N_MAX], int N, int target, int &row, int &col) {
+  return binPart(mat, N, N, target, 0, 0, N-1, N-1, row, col);
+}
+    //https://github.com/anson627/leetcode/blob/master/Searcha2DMatrix/Searcha2DMatrix.cpp
+    bool searchMatrix3(vector<vector<int> > & matrix, int target) {
         int M = matrix.size();
         int N = matrix[0].size();
-        int l = 0, u = M*N-1;
+        int l = 0, u = M*N - 1;
         while (l <= u) {
-            int m = l+(u-l)/2;
-            if (matrix[m/N][m%N] == target) return true;
-            if (matrix[m/N][m%N] < target) l = m+1;
-            else u = m-1;
+            int m = l + (u - l) / 2;
+            if (matrix[m / N][m%N] == target) return true;
+            if (matrix[m / N][m%N] < target) l = m + 1;
+            else u = m - 1;
+        }
+        return false;
+    }
+    
+
+    
+    
+    // my wrong answer!!
+    bool searchBlock(vector<vector<int>> matrix, int target, int m1, int n1, int m, int n) {
+        // do as few recursions as possible.
+        
+        //  /-------\
+        //  | A | B |
+        //  |-------|
+        //  | C | D |
+        //  \-------/
+        
+        while (m1 <= m && n1 <= n) {
+            if (m1 == m && n1 == n) return (target == matrix[m][n]);
+            
+            int m2 = m1 + (m - m1) / 2;
+            int n2 = n1 + (n - n1) / 2;
+
+            // if (target < matrix[m2][n2]) { // could be A
+            //     //   m = m2; n = n2;
+
+            
+            if (target == matrix[m2][n]) return true;
+            if (target < matrix[m2][n]) { // A B not D. BUT C unknown
+                if (n!=n2 && target < matrix[m][n2]) { // get C done
+                    if (searchBlock(matrix, target, m2, n2, m, n2))
+                        return true;
+                }
+                m = m2;  // A and B
+                if (target == matrix[m2][n2])
+                    return true;
+                if (target < matrix[m2][n2]) //go A
+                    n = n2;
+                else // go B
+                    n1 = n2 + 1; 
+            } else { // not A B. either C or D
+                m1 = m2 + 1;
+                if (target == matrix[m][n2]) return true;
+                if (target < matrix[m][n2]) // go C
+                    n = n2; 
+                else n1 = n2 + 1; // go D
+            }
+        
         }
         return false;
     }
 };
-
 int main() {
     Solution sol;
 
